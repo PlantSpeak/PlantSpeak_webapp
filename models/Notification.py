3bdf16b9aa5382758173ded507c2f7fea3f23388
s3685254 <s3685254@student.rmit.edu.au>
2021-04-25 01:14:11 +1000

    Added email notification functionality

--- models/Notification.py
+++ models/Notification.py
@@ -1,25 +1,12 @@
 from flask import current_app
-import flask_mail
-
-mail_server = ""
-mail_port = ""
-admin_email = ""
-admin_email_pwd = ""
+from database import bcrypt, db
+from mail_tool import mail
+from flask_mail import Message
 
 MESSAGE_MAX_LENGTH = 32768
 PHONE_MAX_LENGTH = 16
 MAX_TOPIC_LENGTH = 16
-
-current_app.config.update(
-	MAIL_SERVER=mail_server,
-	MAIL_PORT=mail_port,
-	MAIL_USE_SSL=True,
-	MAIL_USERNAME = admin_email,
-	MAIL_PASSWORD = admin_email_pwd,
-    DEBUG=True
-)
-
-mail = flask_mail.Mail(current_app)
+EMAIL_MAX_LENGTH = 256
 
 # As it stands, only email and push notifications are being implemented.
 class Notification(db.Model):
@@ -27,12 +14,16 @@ class Notification(db.Model):
     topic = db.Column(db.String(MAX_TOPIC_LENGTH))
     message = db.Column(db.String(MESSAGE_MAX_LENGTH))
     email = db.Column(db.String(EMAIL_MAX_LENGTH))
-    phone = db.Column(db.String(PHONE_MAX_LENGTH))
+
+    def __init__(self, topic, message, email):
+        self.topic = topic
+        self.message = message
+        self.email = email
 
     def sendEmail(self):
-        msg = flask_mail.Message(self.topic, recipients=[self.email], sender=admin_email)
+        msg = Message(self.topic, recipients=[self.email], sender='PlantSpeak')
         msg.body = self.message
-        msg.send(self.message)
+        mail.send(msg)
 
     def sendSMS(self):
         return
