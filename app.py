58e8d0753ff5811da3774eef63b31717ee11e01a
s3685254 <s3685254@student.rmit.edu.au>
2021-04-25 04:20:30 +1000

    updated gitignore

--- app.py
+++ app.py
@@ -2,8 +2,13 @@ from flask import Flask, Blueprint, session
 from controllers.UserController import user_pages
 import flask_sqlalchemy
 from database import bcrypt, db
+from mail_tool import mail
 
 from controllers.UserController import *
+
+MESSAGE_MAX_LENGTH = 32768
+PHONE_MAX_LENGTH = 16
+MAX_TOPIC_LENGTH = 16
 
 def create_app():
     application = Flask(__name__, template_folder="views")
@@ -13,6 +18,15 @@ def create_app():
 
     bcrypt.init_app(application)
     db.init_app(application)
+
+    application.config['MAIL_SERVER'] = 'smtp.mailtrap.io'
+    application.config['MAIL_PORT'] = 2525
+    application.config['MAIL_USERNAME'] = '06f82b5867a132'
+    application.config['MAIL_PASSWORD'] = '1fb024fddad45a'
+    application.config['MAIL_USE_TLS'] = True
+    application.config['MAIL_USE_SSL'] = False
+
+    mail.init_app(application)
 
     application.register_blueprint(user_pages)
     return application
