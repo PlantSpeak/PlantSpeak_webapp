45783eb611ebb4c15a0a89b466bc03e36355a347
JustGeoffrey <49662399+JustGeoffrey@users.noreply.github.com>
2021-04-25 17:38:23 +1000

    Updated app.py to create the database in the project directory, and added plant blueprint to project

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, Blueprint, session
 from controllers.UserController import user_pages
+from controllers.PlantController import plant_pages
 import flask_sqlalchemy
 from database import bcrypt, db
 
@@ -8,13 +9,14 @@ from controllers.UserController import *
 def create_app():
     application = Flask(__name__, template_folder="views")
 
-    application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'  # REPLACE THIS.
+    application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'  # REPLACE THIS.
     application.secret_key = "VERY SECRET KEY"  # Update for production.
 
     bcrypt.init_app(application)
     db.init_app(application)
 
     application.register_blueprint(user_pages)
+    application.register_blueprint(plant_pages)
     return application
 
 application = create_app()
