fd291e6b97c0dc065def98e46cb6d84ad4910119
s3724076 <37397303+s3724076@users.noreply.github.com>
2021-04-26 15:09:25 +1000

    Uploaded Plant Type Controller

---
+++ controllers/PlantTypeController.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, current_app, url_for, request, redirect, render_template, session
+from models.Plant_Type import *
+from database import db
+from wtforms import Form, BooleanField, StringField, PasswordField, validators
+import flask_bcrypt
+
+plant_type_pages = Blueprint('plant types', __name__, template_folder="views")
+
+class RegistrationForm(Form):
+    #plantname = StringField('Plant Name', [validators.Length(min=NAME_MIN_LENGTH, max=NAME_MAX_LENGTH)])
+    level = StringField('Plant Level', [validators.Length(min=LEVEL_MIN_LENGTH, max=LEVEL_MAX_LENGTH)])
+    location = StringField('Plant Location', [validators.Length(min=LOCATION_MIN_LENGTH, max=LOCATION_MAX_LENGTH)])
+
+
+@plant_type_pages.route('/register/planttype', methods=['GET','POST'])
+def register():
+    form = RegistrationForm(request.form)
+    if request.method=="POST" and form.validate():
+        newPlant = Plant(level =  form.level.data, location=form.location.data)
+        db.session.add(newPlant)
+        db.session.commit()
+        #session['username'] = newUser.username
+        return redirect(url_for('home'))
+    return render_template("register.html", form=form)
